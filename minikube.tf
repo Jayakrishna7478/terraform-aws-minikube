ceea710f3ea3685051add24af1bcca76a9c69992
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-02-25 10:08:22 +0530

    terraform-aws-minikube

---
+++ minikube.tf
@@ -0,0 +1,23 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/roboshop.pub"
+  aws_subnet_id = "subnet-0c625bb923eb68356"
+  #ami_image_id = "ami-b81dbfc5"
+  hosted_zone = "kubernete.online"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
